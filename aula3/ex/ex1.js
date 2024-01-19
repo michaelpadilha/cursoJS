ed6b9ab895b066f740f1d7dce7993b9d894ab3f7
Michael Padilha <michael.padilha96@gmail.com>
2024-01-19 20:33:37 -0300

    Conhecendo o console.log()

---
+++ aula3/ex/ex1.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é "Michael". Estou aprendendo JavaScript às', 8, 'hrs da noite.');
---
+++ aula3/index.js
@@ -0,0 +1,5 @@
+console.log('Michael "Padilha"');
+console.log("Michael 'Padilha'");
+console.log(`"Michael" 'Padilha'`);
+
+console.log(123456, 15.85465456, 'Michael Padilha');
