b2f803d52a3204f5dd43312b5e07866fb41e279e
Michael Padilha <michael.padilha96@gmail.com>
2024-01-19 22:59:33 -0300

    Comentários no JS

---
+++ aula4/comentarios.js
@@ -0,0 +1,16 @@
+// Comentário
+console.log('Olá mundo!'); // Aqui tem outro comentário
+console.log('linha 1');
+
+/*
+Um bloco de código
+Da pra escrever mais
+* Lorem
+* Lorem
+*/
+
+console.log('linha 2');
+//console.log('linha 3');
+console.log('linha 4');
+// Vai ser ignorado
+// Comentário de linha
