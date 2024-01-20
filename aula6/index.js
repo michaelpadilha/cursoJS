4585a7eca111919753e3c6048e738358872d2fa7
Michael Padilha <michael.padilha96@gmail.com>
2024-01-20 16:52:21 -0300

    Variáveis let

---
+++ aula6/index.js
@@ -0,0 +1,27 @@
+let nome = 'Michael'; // String
+
+console.log(nome, 'nasceu em 1996.');
+console.log('Em 2015', nome, 'conheceu Cynthia.');
+console.log(nome, 'casou-se com Cynthia em 2016.');
+console.log('Cynthia teve 1 filha com', nome, 'em 2018.');
+console.log('A filha de', nome, 'se chama Mariana');
+
+let sobrenome; // Declarou a variável
+sobrenome = 'Juliano'; //Iniciando a variável
+console.log(sobrenome);
+sobrenome = 'Padilha';
+console.log(sobrenome);
+
+// Não podemos criar variáveis com palavras reservadas
+
+// Variáveis precisam ter nomes significativos ex: nomeCliente -> nc(sem sentido)
+
+// Não pode começar o nome de uma variável com número
+
+// Não podem conter espaços ou traços
+
+// Utilizar camelCase
+
+// Case-sensitive (letras maiúsculas e minúsculas fazem diferença, ex nomecliente e nomeCliente)
+
+// NÃO UTILIZE VAR, UTILIZE LET.
