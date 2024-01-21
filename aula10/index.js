c306a8fe4ebb457d6a2a19c67fe463c9db825328
Michael Padilha <michael.padilha96@gmail.com>
2024-01-21 14:58:34 -0300

    Tipos de dados primitivos

---
+++ aula10/index.js
@@ -0,0 +1,14 @@
+const nome = 'Michael'; // String
+const nome1 = "Michael"; // String
+const nome2 = `Michael`; // String
+const num1 = 10; // Number
+const num2 = 10.52; // Number
+let nomeAluno; // Undefined -> não aponta para local nenhum na memória
+let sobrenomeAluno = null; // Nulo -> não aponta para local nenhum na memória
+const aprovado = true; // Boolean -> true ou false (lógico)
+
+console.log(typeof nome, nome);
+console.log(typeof num1, num1);
+console.log(typeof nomeAluno, nomeAluno);
+console.log(typeof sobrenomeAluno, sobrenomeAluno);
+console.log(typeof aprovado, aprovado);
