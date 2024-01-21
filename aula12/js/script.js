4cf259d5477497ad6dbd6c23fd720c203d13a04b
Michael Padilha <michael.padilha96@gmail.com>
2024-01-21 16:24:59 -0300

    alert, confirm e prompt + exercício

---
+++ aula12/js/script.js
@@ -0,0 +1,13 @@
+let num1 = prompt('Digite um número:');
+let num2 = prompt('Digite outro número:');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const soma = num1 + num2;
+
+alert(soma);
+
+confirm('Está correto?')
+
+// alert(), confirm(), prompt()
