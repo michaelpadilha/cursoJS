cf17207b0761e8746cc8e2df749cf3127f1a2cde
Michael Padilha <michael.padilha96@gmail.com>
2024-01-20 22:12:53 -0300

    Constantes com const

---
+++ aula7/index.js
@@ -0,0 +1,21 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não podemos começar o nome de uma constante com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não pode modificar o valor de uma constante
+// NÃO UTILIZE VAR, UTILIZE CONST.
+
+const nome = 'Michael';
+console.log(nome);
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+console.log(resultadoDuplicado);
+
+// + - * /
+
+console.log(typeof primeiroNumero);
