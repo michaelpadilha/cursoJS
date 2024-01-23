7acd11a45839ef484124904e3962809069b4e01f
Michael Padilha <michael.padilha96@gmail.com>
2024-01-23 15:47:47 -0300

    Arrays (básico)

---
+++ aula16/index.js
@@ -0,0 +1,23 @@
+//               0          1          2
+const alunos = ['Michael', 'Cynthia', 'Mariana'];
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+// console.log(alunos.slice(0, -2));
+
+// delete alunos[1]; REMOVE SEM ALTERAR OS INDICES
+// const removido = alunos.shift(); REMOVE DO COMEÇO
+// const removido = alunos.pop(); REMOVE DO FINAL
+
+
+// alunos.unshift('Erick'); ADICIONA NO COMEÇO
+// alunos.push('Erick'); ADICIONA NO FIM
+// alunos[alunos.length] = 'Erick';
+// alunos[3] = 'Irani';
+
+// alunos[0] = 'Juliano'; ALTERA
+
+// console.log(alunos[1]);
+// console.log(alunos[0]);
+// console.log(alunos);
