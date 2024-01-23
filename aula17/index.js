d889d3f509ca6c20d71713b39be10b84fb0b947a
Michael Padilha <michael.padilha96@gmail.com>
2024-01-23 18:14:56 -0300

    Funções (básico)

---
+++ aula17/index.js
@@ -0,0 +1,22 @@
+/** 
+ * MODO CLÁSSICO DE DECLARAR UMA FUNCTION
+ * 
+function soma(x = 1, y = 1) {
+    const resultado = x + y;
+    return resultado;
+}
+
+const resultado = soma (2, 2);
+console.log(resultado);
+*/
+
+/** const raiz = function (n) {
+return n ** 0.5;
+};
+*/
+
+const raiz = n => n ** 0.5;
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
